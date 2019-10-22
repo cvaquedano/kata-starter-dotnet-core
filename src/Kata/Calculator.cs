2cc2dfc18e7634a20a1e1e3b0059542d26e5d53a
Carlos Vaquedano <chvb2002@hotmail.com>
2019-10-22 15:24:47 -0600

    greater than 1000 pasing, multiple character delimiter failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -21,7 +21,7 @@ namespace Kata
                 s = splittedText[1];
             }
 
-            var numbers = s.Split(delimiters, StringSplitOptions.None).Select((int.Parse));
+            var numbers = s.Split(delimiters, StringSplitOptions.None).Select((int.Parse)).Where(x=> x <1001);
 
             var negatives = numbers.Where(x => x < 0);
 
