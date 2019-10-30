029bf42fcae094a772cc17b6e6930c44599b7b0f
Edward Siwady <siwady0908@gmail.com>
2019-10-30 14:49:55 -0700

    one negative passing, multi negative failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -27,6 +27,11 @@ namespace Kata
             
             var numbers = s.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
 
+            var negatives = numbers.Where(x => x < 0);
+            if (negatives.Count() == 1)
+            {
+                throw new Exception("negatives not allowed: " + negatives.First());
+            }
             return numbers.Sum();
         }
     }
