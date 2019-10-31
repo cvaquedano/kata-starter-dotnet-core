64ee9a7d97c9290e08d7ed01895a9772e04e8272
Edward Siwady <siwady0908@gmail.com>
2019-10-31 10:18:30 -0700

    custom delimiter passing, one negative failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,12 +7,22 @@ namespace Kata
     {
         public int Add(string s = "")
         {
+            var delimiters = new []{"\n",","};
             if (string.IsNullOrEmpty(s))
             {
                 return 0;
             }
 
-            var numbers = s.Split(new []{"\n",","},StringSplitOptions.None).Select(int.Parse);
+            if (s.Contains("/"))
+            {
+                var textSplitted = s.Split("\n");
+                var delimiter = textSplitted[0].Replace("/","");
+                s = textSplitted[1];
+                delimiters = new[] {delimiter};
+            }
+
+            
+            var numbers = s.Split(delimiters,StringSplitOptions.None).Select(int.Parse);
 
             return numbers.Sum();
         }
