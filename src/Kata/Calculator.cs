def93a3237bc8a983e7a3d707c8c2ae9dd376127
Carlos Vaquedano <chvb2002@hotmail.com>
2019-10-31 11:10:03 -0600

    two numbers passing, unknow number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -9,7 +11,14 @@ namespace Kata
                 return 0;
             }
 
-            return 1;
+            var numbers = s.Split(",").Select(int.Parse);
+
+            if (numbers.Count()==1)
+            {
+                return 1;
+            }
+
+            return numbers.First() + numbers.Last();
         }
     }
 }
